2289eeec6e77d243a16b43b3486d6929db1cb413
liuyongsheng <ilylys@163.com>
2017-06-22 15:35:31 +0800

    what: 新增原型链 Why: How:

---
+++ JS/prototype.js
@@ -0,0 +1,39 @@
+/*
+    从原型到原型链
+*/
+function Person() {
+
+}
+var person = new Person();
+person.name = 'Kevin';
+console.log(person.name) // Kevin
+
+//使用new创建了一个实例对象
+
+function Person() {
+
+}
+// 虽然写在注释里，但是你要注意：
+// prototype是函数才会有的属性
+Person.prototype.name = 'Kevin';
+var person1 = new Person();
+var person2 = new Person();
+console.log(person1.name) // Kevin
+console.log(person2.name) // Kevin
+
+// _proto_
+// 每一个对象都会具有的一个属性， 会指向该对象的原型
+function Person() {
+
+}
+var person = new Person();
+console.log(person.__proto__ === Person.prototype); // true
+// 实例对象和构造函数都可以指向原型，原型的属性constructor 是可以指向构造函数的
+function Person() {
+
+}
+console.log(Person === Person.prototype.constructor); // true
+
+
+console.log(Object.prototype.__proto__ === null) // true
+    // null代表没有对象
